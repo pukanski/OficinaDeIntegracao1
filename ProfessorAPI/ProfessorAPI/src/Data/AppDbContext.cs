c4cf9b23731ac3b8a980296f7e8fa2975e43fb63
Luiz Fernando <luizfmd21@gmail.com>
2026-05-10 20:06:43 -0300

    Desenvolvimento ProfessorAPI

---
+++ ProfessorAPI/ProfessorAPI/src/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProfessorAPI.src.Model;
+
+namespace ProfessorAPI.src.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+                
+            public DbSet<Professor> Professores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Garante unicidade de SIAPE e Email no banco
+            modelBuilder.Entity<Professor>()
+                .HasIndex(p => p.siape)
+                .IsUnique();
+
+            modelBuilder.Entity<Professor>()
+                .HasIndex(p => p.email)
+                .IsUnique();
+        }
+    }
+
+}
+
